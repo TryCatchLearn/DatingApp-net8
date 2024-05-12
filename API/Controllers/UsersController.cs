0e38358f9440da508f12b508cf23d3eff4c19465
Neil Cummings <31762828+TryCatchLearn@users.noreply.github.com>
2024-05-12 14:17:53 +0700

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]  // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
