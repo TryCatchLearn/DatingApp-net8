32cf523d6f29ce74ccfd83255ba442882be813b1
Neil Cummings <31762828+TryCatchLearn@users.noreply.github.com>
2024-06-12 13:25:26 +0700

    Roles fix

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,9 +18,10 @@ export class AccountService {
   roles = computed(() => {
     const user = this.currentUser();
     if (user && user.token) {
-      return JSON.parse(atob(user.token.split('.')[1])).role
+      const role = JSON.parse(atob(user.token.split('.')[1])).role;
+      return Array.isArray(role) ? role : [role];
     }
-    return null;
+    return [];
   })
 
   login(model: any) {
